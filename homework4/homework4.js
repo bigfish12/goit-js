3a844153ccbaf1296bdb85f794bfd1a05cf87fe3
bigfish12 <2023felixs@psi.kiev.ua>
2020-05-14 09:51:48 +0300

    Homework 4 - Done

--- homework4/homework4.js
+++ homework4/homework4.js
@@ -35,30 +35,32 @@ console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order
 // Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
 
 const inventory = {
-  items: ['Knife', 'Gas mask'],
+  items: ["Knife", "Gas mask"],
   add(itemName) {
     console.log(`Adding ${itemName} to inventory`);
+
     this.items.push(itemName);
   },
-
   remove(itemName) {
     console.log(`Removing ${itemName} from inventory`);
-    this.items = this.items.filter(item => item !== itemName);
+
+    this.items = this.items.filter((item) => item !== itemName);
   },
 };
 
-
-const invokeInventoryAction = function(itemName, callback) {
+const invokeInventoryAction = function (itemName, itemAction) {
   console.log(`Invoking action on ${itemName}`);
-  callback(itemName);
+  itemAction(itemName);
 };
 
-invokeInventoryAction('Medkit', inventory.add);
-// // Invoking action on Medkit
-// // Adding Medkit to inventory
-// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
-// invokeInventoryAction('Gas mask', inventory.remove);
-// // Invoking action on Gas mask
-// // Removing Gas mask from inventory
+invokeInventoryAction("Bat", inventory.add.bind(inventory));
+// Invoking action on Medkit
+// Adding Medkit to inventory
 
-// console.log(inventory.items); // ['Knife', 'Medkit']
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction("Gas mask", inventory.remove.bind(inventory));
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
